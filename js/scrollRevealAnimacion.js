988b8c0b5fb9141788f832c275f5f86224411ca3
ServiciosIntegralesMartinezR <serviciosintegrales.martinez.r@gmail.com>
2025-04-09 21:40:35 -0400

    Primera Subida de pagina web

---
+++ js/scrollRevealAnimacion.js
@@ -0,0 +1,45 @@
+ScrollReveal().reveal('.section-title', {
+    delay: 200,
+    duration: 2000,
+    distance:'50%',
+    origin:'top',
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    reset: true
+});
+
+ScrollReveal().reveal('.titulo', {
+    delay: 200,
+    duration: 2000,
+    distance:'50%',
+    origin:'top',
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    reset: true
+});
+
+ScrollReveal().reveal('.galeria', {
+    delay: 200,
+    duration: 2000,
+    origin:'top',
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    reset: true
+});
+
+
+ScrollReveal().reveal('.left', {
+    delay: 200,
+    duration: 2000,
+    distance:'50%',
+    origin:'left',
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    reset: true
+});
+
+ScrollReveal().reveal('.right', {
+    delay: 200,
+    duration: 2000,
+    distance:'50%',
+    origin:'right',
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    reset: true
+});
+
